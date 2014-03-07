72458164d441f041d98b482cc215abec724e7c61
Bryan Boettcher <bboettcher@thevibrand.com>
2014-03-07 15:11:07 -0600

    functioning project with test

---
+++ ConstructorMapper.Tests/MappingTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using NUnit.Framework;
+using Pingring.Utilities;
+
+namespace Pingring.Utilities.ConstructorMapper.Tests
+{
+    public abstract class BaseTestHarness
+    {
+        [SetUp]
+        protected void Setup()
+        {
+            Arrange();
+            Act();
+        }
+
+        [TearDown]
+        protected virtual void Cleanup() { }
+
+        protected abstract void Arrange();
+        protected abstract void Act();
+    }
+
+    public class MappingTests : BaseTestHarness
+    {
+        private Poco _dbModel;
+        private BusinessObject _businessObject;
+
+        protected override void Arrange()
+        {
+            _dbModel = new Poco
+            {
+                user_id = 13,
+                user_name = "bboettcher",
+                comment_count = 42,
+                display_name = "Bryan Boettcher",
+                registered_date = new DateTime(2014, 03, 07)
+            };
+
+            ConstructorMapper.Configure<Poco, BusinessObject>();
+        }
+
+        protected override void Act()
+        {
+            _businessObject = ConstructorMapper.Map<Poco, BusinessObject>(_dbModel);
+        }
+        
+        [Test]
+        public void It_will_map_properties()
+        {
+            Assert.That(_businessObject.UserId, Is.EqualTo(13));
+            Assert.That(_businessObject.UserName, Is.EqualTo("bboettcher"));
+            Assert.That(_businessObject.CommentCount, Is.EqualTo(42));
+            Assert.That(_businessObject.DisplayName, Is.EqualTo("Bryan Boettcher"));
+            Assert.That(_businessObject.RegisteredDate, Is.EqualTo(new DateTime(2014, 03, 07)));
+        }
+
+        protected override void Cleanup()
+        {
+            ConstructorMapper.Clear();
+        }
+    }
+
+    public class Poco
+    {
+        public int user_id { get; set; }
+        public string user_name { get; set; }
+        public int comment_count { get; set; }
+        public DateTime registered_date { get; set; }
+        public string display_name { get; set; }
+    }
+
+    public class BusinessObject
+    {
+        private readonly int _userId;
+        private readonly string _userName;
+        private readonly int _commentCount;
+        private readonly DateTime _registeredDate;
+        private readonly string _displayName;
+
+        public BusinessObject(int userId, string userName, int commentCount, DateTime registeredDate, string displayName)
+        {
+            _userId = userId;
+            _userName = userName;
+            _commentCount = commentCount;
+            _registeredDate = registeredDate;
+            _displayName = displayName;
+        }
+
+        public int UserId { get { return _userId; } }
+        public string UserName { get {  return _userName; } }
+        public int CommentCount { get { return _commentCount; } }
+        public DateTime RegisteredDate { get { return _registeredDate; } }
+        public string DisplayName { get { return _displayName; } }
+    }
+}
---
+++ ConstructorMapper/ConstructorMapper.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Pingring.Utilities.ConstructorMapper
+{
+    public static class ConstructorMapper
+    {
+        private static readonly IDictionary<TypePair, object> Mappings;
+
+        static ConstructorMapper()
+        {
+            Mappings = new Dictionary<TypePair, object>();
+        }
+
+        public static void Clear()
+        {
+            Mappings.Clear();
+        }
+
+        public static void Configure<TIn, TOut>()
+        {
+            var key = new TypePair(typeof(TIn), typeof(TOut));
+
+            var inputParameter = Expression.Parameter(typeof(TIn));
+            var destinationConstructor = GetIdealConstructor<TOut>();
+
+            var sourceProperties = typeof(TIn).GetProperties();
+
+            var destinationConstructorArguments = (from destinationConstructorParameter in destinationConstructor.GetParameters()
+                                                   let sourceProperty = sourceProperties.FirstOrDefault(pi => IsCorrectProperty(pi, destinationConstructorParameter))
+                                                   select sourceProperty != null
+                                                       ? (Expression)Expression.Property(inputParameter, sourceProperty.Name)
+                                                       : (Expression)Expression.Default(destinationConstructorParameter.ParameterType));
+
+            // "new TOutput(x, y, z)"
+            var constructionExpression = Expression.New(destinationConstructor, destinationConstructorArguments);
+
+            // old => new TOutput(old.x, old.y, old.z)
+            var converterExpression = Expression.Lambda<Func<TIn, TOut>>(constructionExpression, inputParameter);
+
+            var converterLambda = converterExpression.Compile();
+            Mappings.Add(key, converterLambda);
+        }
+
+        private static bool IsCorrectProperty(PropertyInfo pi, ParameterInfo destinationConstructorParameter)
+        {
+            return pi.PropertyType.IsAssignableFrom(destinationConstructorParameter.ParameterType) &&
+                   (NamesMatch(pi.Name, destinationConstructorParameter.Name));
+        }
+
+        private static bool NamesMatch(string a, string b)
+        {
+            Func<string, string> sanitize = s => s.ToLowerInvariant().Replace("_", "");
+
+            return sanitize(a) == sanitize(b);
+        }
+
+        private static ConstructorInfo GetIdealConstructor<TOut>()
+        {
+            return typeof(TOut).GetConstructors().OrderByDescending(ci => ci.GetParameters().Count()).First();
+        }
+
+        public static TOut Map<TIn, TOut>(TIn input)
+        {
+            var key = new TypePair(typeof(TIn), typeof(TOut));
+
+            if (!Mappings.ContainsKey(key))
+                throw new InvalidOperationException("No mapping defined");
+
+            var converter = (Func<TIn, TOut>)Mappings[key];
+            return converter(input);
+        }
+
+        class TypePair : IEquatable<TypePair>
+        {
+            public bool Equals(TypePair other)
+            {
+                if (ReferenceEquals(null, other))
+                    return false;
+                if (ReferenceEquals(this, other))
+                    return true;
+                return T1.Equals(other.T1) && T2.Equals(other.T2);
+            }
+
+            public override bool Equals(object obj)
+            {
+                if (ReferenceEquals(null, obj))
+                    return false;
+                if (ReferenceEquals(this, obj))
+                    return true;
+                if (obj.GetType() != this.GetType())
+                    return false;
+                return Equals((TypePair)obj);
+            }
+
+            public override int GetHashCode()
+            {
+                unchecked
+                {
+                    return (T1.GetHashCode() * 397) ^ T2.GetHashCode();
+                }
+            }
+
+            public static bool operator ==(TypePair left, TypePair right)
+            {
+                return Equals(left, right);
+            }
+
+            public static bool operator !=(TypePair left, TypePair right)
+            {
+                return !Equals(left, right);
+            }
+
+            public TypePair(Type t1, Type t2)
+            {
+                T1 = t1;
+                T2 = t2;
+            }
+
+            public readonly Type T1;
+            public readonly Type T2;
+        }
+    }
+}
